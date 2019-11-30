9687f0fb33c71f3f5be606dbb407a41c993de090
Kenneth Gulbrandsoy <kenneth@discoos.org>
2019-11-30 13:41:38 +0100

    Added minimal Aqueduct server implementation

---
+++ lib/sarsys_app_server.dart
@@ -0,0 +1,11 @@
+/// sarsys_app_server
+///
+/// A Aqueduct web server.
+library sarsys_app_server;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
