101c0f48280a342fae790724eba80437a1796920
Kenneth Gulbrandsoy <kenneth@discoos.org>
2019-12-26 12:39:22 +0100

    Removed test

--- test/app_config_test.dart
+++ test/app_config_test.dart
@@ -10,8 +10,4 @@ Future main() async {
   test("GET /api/healthz returns status code 200 Status OK", () async {
     expectResponse(await harness.agent.get("/api/healthz"), 200, body: "Status OK");
   });
-
-  test("GET /app-config/:id returns 200 GET /api/app-config/:id", () async {
-    expectResponse(await harness.agent.get("/api/app-config/1"), 200, body: 'GET /api/app-config/1');
-  });
 }
