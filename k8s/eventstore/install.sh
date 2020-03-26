a855ac61397ba1f687d75568c8898c5c7e5e1696
Kenneth Gulbrandsoy <kenneth@discoos.org>
2020-03-26 16:44:46 +0100

    Increased heartbeat intervals adapted for cloud

--- k8s/eventstore/install.sh
+++ k8s/eventstore/install.sh
@@ -5,7 +5,11 @@ helm repo add eventstore https://eventstore.github.io/EventStore.Charts
 helm repo update
 echo "[✓] Helm updated"
 
-echo "2) Install EventStore"
+echo "2) Install Storage Class 'do-block-storage-retain' (default class does not retain)"
+kubectl -n sarsys get storageclasses
+kubectl -n sarsys apply -f storageclass.yaml
+
+echo "3) Install EventStore"
 helm install --namespace sarsys -n eventstore eventstore/eventstore -f eventstore.yaml
 kubectl -n sarsys rollout status statefulset eventstore
 echo "[✓] EventStore installed"
--- k8s/eventstore/status.sh
+++ k8s/eventstore/status.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-echo "Get eventstore status"
+echo "Get eventstore status - rollout"
 kubectl -n sarsys rollout status statefulset eventstore
+
+echo "Get eventstore status - rollout history"
 kubectl -n sarsys rollout history statefulset eventstore
+
+# Howto resize PVC see https://github.com/EventStore/EventStore.Charts/tree/master/stable/eventstore#option-1-resize-pvc-created-with-volume-expansion-enabled
+
+echo "Get eventstore status - free space | eventstore-0"
+kubectl -n sarsys exec eventstore-0 df
+
+echo "Get eventstore status - free space | eventstore-1"
+kubectl -n sarsys exec eventstore-1 df
+
+echo "Get eventstore status - free space | eventstore-2"
+kubectl -n sarsys exec eventstore-2 df
+
 echo "[✓] EventStore status completed"
