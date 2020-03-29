e05f12e4f58da03f02c0428df70ff5e315c3b221
Kenneth Gulbrandsoy <kenneth@discoos.org>
2020-03-29 22:02:31 +0200

    Added manual steps to eventstore uninstall.sh

--- k8s/eventstore/uninstall.sh
+++ k8s/eventstore/uninstall.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-echo "Uninstall eventstore"
+echo "1) Uninstall eventstore"
 helm delete eventstore --purge
+
+echo "2) Manually delete PersistentVolumeClaims"
+kubectl -n sarsys get pvc | grep data-eventstore-
+
+echo "3) Manually delete PersistentVolumes"
+kubectl -n sarsys get pv | grep sarsys/data-eventstore-
+
 echo "[✓] EventStore uninstalled"
