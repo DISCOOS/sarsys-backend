4ff2f04e896bcb09b8625dd96593d5951b024d7b
Kenneth Gulbrandsoy <kenneth@discoos.org>
2021-01-30 14:07:47 +0100

    Added system status controller

--- sarsys_ops_server/lib/src/k8s/k8s_api.dart
+++ sarsys_ops_server/lib/src/k8s/k8s_api.dart
@@ -6,13 +6,14 @@ import 'package:logging/logging.dart';
 import 'package:sarsys_ops_server/sarsys_ops_server.dart';
 import 'package:stack_trace/stack_trace.dart';
 
-// ignore: camel_case_types
 class K8sApi {
   static const _serviceAccountPath = '/var/run/secrets/kubernetes.io/serviceaccount';
   final logger = Logger('$K8sApi');
   final tokenFile = File('$_serviceAccountPath/token');
   final certFile = File('$_serviceAccountPath/ca.crt');
   final serviceAccountDir = Directory(_serviceAccountPath);
+  String get namespace => Platform.environment['POD_NAMESPACE'];
+  bool get isReady => tokenFile.existsSync() && certFile.existsSync();
 
   HttpClient get client {
     if (certFile.existsSync()) {
@@ -43,19 +44,15 @@ class K8sApi {
     logger.info('K8S api: CERT: ${certFile.existsSync() ? 'Found' : 'Not found'}');
     logger.info('K8S api: TOKEN: ${tokenFile.existsSync() ? 'Found' : 'Not found'}');
 
-    if (tokenFile.existsSync() && certFile.existsSync()) {
-      logger.info('K8S api: checking...');
-      final token = tokenFile.readAsStringSync();
-      final url = Uri.parse('https://$apiHost:$apiPort/api');
-      final request = await client.getUrl(url);
-      logger.info('K8S api: request: ${request.method} ${request.uri}');
+    if (isReady) {
+      logger.info('K8S api: Check...');
+
       try {
-        request.headers.add('Authorization', 'Bearer $token');
-        final response = await request.close();
+        final response = await get('/api');
         logger.info('K8S api: ${response.statusCode} ${response.reasonPhrase}');
-        final json = await toBody(response);
+        final json = await toContent(response);
         ok = json != null;
-        logger.info('K8S api: content: $ok');
+        logger.info('K8S api: Has content: $ok');
       } on Exception catch (error, stackTrace) {
         logger.severe(
           'K8S api: error: $error',
@@ -63,12 +60,56 @@ class K8sApi {
           stackTrace ?? Trace.current(1),
         );
       }
-      logger.info('K8S api: checking...done');
+      logger.info('K8S api: Check...done');
     }
     return ok;
   }
 
-  static Future<dynamic> toBody(HttpClientResponse response) async {
+  Future<List<String>> getPodNamesFromNs(String ns) async {
+    final pods = <String>[];
+
+    if (isReady) {
+      logger.fine(
+        'K8S api: ${Context.toMethod('getPodNamesFromNs', ['namespace: $ns'])}...',
+      );
+      try {
+        final response = await get('/api/v1/namespaces/$ns/pods');
+        logger.info('K8S api: ${response.statusCode} ${response.reasonPhrase}');
+        final json = Map<String, dynamic>.from(await toContent(response));
+        logger.fine('K8S api: $json');
+        final items = json.mapAt<String, Map<String, Map<String, dynamic>>>('items')?.values;
+        if (items != null) {
+          pods.addAll(
+            items.map((pod) => pod['metadata'].elementAt<String>('name')),
+          );
+        }
+        logger.fine('K8S api: pods: $pods');
+      } on Exception catch (error, stackTrace) {
+        logger.severe(
+          'K8S api: ${Context.toMethod('getPodNamesFromNs', [
+            'namespace: $ns',
+            'error: $error',
+          ])}',
+          error,
+          stackTrace ?? Trace.current(1),
+        );
+      }
+      logger.fine(
+        'K8S api: ${Context.toMethod('getPodNamesFromNs', ['namespace: $ns'])}...done',
+      );
+    }
+    return pods;
+  }
+
+  Future<HttpClientResponse> get(String uri) async {
+    final url = Uri.parse('https://$apiHost:$apiPort$uri');
+    final request = await client.getUrl(url);
+    logger.fine('K8S api: request: ${request.method} ${request.uri}');
+    request.headers.add('Authorization', 'Bearer $token');
+    return request.close();
+  }
+
+  static Future<dynamic> toContent(HttpClientResponse response) async {
     final completer = Completer<String>();
     final contents = StringBuffer();
     response.transform(utf8.decoder).listen(
