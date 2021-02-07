cd8784ee845f4e6447f9957b0d5560435776ebf4
Kenneth Gulbrandsoy <kenneth@discoos.org>
2021-02-07 22:13:10 +0100

    Added module port number to pod urls

--- sarsys_ops_server/lib/src/controllers/module_status_controller.dart
+++ sarsys_ops_server/lib/src/controllers/module_status_controller.dart
@@ -4,12 +4,17 @@ import 'package:sarsys_ops_server/src/controllers/status_base_controller.dart';
 
 class ModuleStatusController extends StatusBaseController {
   ModuleStatusController(SarSysOpsConfig config)
-      : super('Module', config, [
+      : ports = {
+          'sarsys-app-server': config.app.port,
+          'sarsys-tracking-server': config.tracking.healthPort,
+        },
+        super('Module', config, [
           'sarsys-app-server',
           'sarsys-tracking-server',
         ]);
 
   final K8sApi k8s = K8sApi();
+  final Map<String, int> ports;
   final HttpClient client = HttpClient();
 
   @override
@@ -28,30 +33,38 @@ class ModuleStatusController extends StatusBaseController {
     );
     return {
       'name': name,
-      'instances': await _toInstanceStatus(pods),
+      'instances': await _toInstanceStatus(
+        name,
+        pods,
+      ),
     };
   }
 
-  Future<List<Map<String, dynamic>>> _toInstanceStatus(List<Map<String, dynamic>> pods) async {
+  Future<List<Map<String, dynamic>>> _toInstanceStatus(String name, List<Map<String, dynamic>> pods) async {
     final instances = <Map<String, dynamic>>[];
     for (var pod in pods) {
       instances.add({
         'name': pod.elementAt('metadata/name'),
-        'status': await _toPodStatus(pod),
+        'status': await _toPodStatus(name, pod),
       });
     }
     return instances.toList();
   }
 
-  Future<Map<String, dynamic>> _toInstanceHealth(Map<String, dynamic> pod) async {
+  Future<Map<String, dynamic>> _toInstanceHealth(String name, Map<String, dynamic> pod) async {
     return {
-      'alive': await _isOK(pod, '/api/healthz/alive'),
-      'ready': await _isOK(pod, '/api/healthz/ready'),
+      'alive': await _isOK(name, pod, '/api/healthz/alive'),
+      'ready': await _isOK(name, pod, '/api/healthz/ready'),
     };
   }
 
-  Future<bool> _isOK(Map<String, dynamic> pod, String uri) async {
-    final url = k8s.toPodUri(pod, uri: uri);
+  Future<bool> _isOK(String name, Map<String, dynamic> pod, String uri) async {
+    final url = k8s.toPodUri(
+      pod,
+      uri: uri,
+      deployment: name,
+      port: ports[name],
+    );
     try {
       final response = await k8s.getUrl(
         url,
@@ -65,14 +78,17 @@ class ModuleStatusController extends StatusBaseController {
     }
   }
 
-  Future<Map<String, dynamic>> _toPodStatus(Map<String, dynamic> pod) async {
+  Future<Map<String, dynamic>> _toPodStatus(String name, Map<String, dynamic> pod) async {
     final conditions = pod.listAt(
       'status/conditions',
       defaultList: [],
     ).map((c) => Map<String, dynamic>.from(c));
 
     return {
-      'health': await _toInstanceHealth(pod),
+      'health': await _toInstanceHealth(
+        name,
+        pod,
+      ),
       'conditions': conditions.isNotEmpty
           ? conditions.toList()
           : [
