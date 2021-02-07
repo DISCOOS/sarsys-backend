4b6e45f56b8137e6e10c3803260cde878fea5e5b
Kenneth Gulbrandsoy <kenneth@discoos.org>
2021-02-07 20:00:15 +0100

    Implemented pod status and server instance health checks

--- sarsys_ops_server/lib/src/schemas.dart
+++ sarsys_ops_server/lib/src/schemas.dart
@@ -6,23 +6,41 @@ import 'package:sarsys_http_core/sarsys_http_core.dart';
 
 APISchemaObject documentModuleStatus() => APISchemaObject.object(
       {
-        "name": APISchemaObject.string()..description = "Module name",
+        'name': APISchemaObject.string()..description = 'Module name',
         'instances': APISchemaObject.array(
           ofSchema: APISchemaObject.object({
-            "name": APISchemaObject.string()..description = "Server instance name",
-            "health": APISchemaObject.object({
-              'alive': APISchemaObject.boolean()..description = 'Server is alive',
-              'ready': APISchemaObject.boolean()..description = 'Server is ready',
+            'name': APISchemaObject.string()..description = 'Server instance name',
+            'status': APISchemaObject.object({
+              'type': APISchemaObject.string()..description = 'Server status type',
+              'acceptable': APISchemaObject.string()
+                ..description = 'Indicates whether that '
+                    'status is applicable, with possible '
+                    'values "True" and "False"',
+              'reason': APISchemaObject.string()
+                ..description = 'Machine-readable, '
+                    'UpperCamelCase text indicating the reason '
+                    "for the status's last transition'",
+              'message': APISchemaObject.string()
+                ..description = 'Human-readable message '
+                    'indicating details about the last '
+                    'status transition.',
             })
+              ..description = 'Server instance status object',
+            'health': APISchemaObject.object({
+              'alive': APISchemaObject.boolean()..description = 'Server instance is alive',
+              'ready': APISchemaObject.boolean()..description = 'Server instance is ready',
+            })
+              ..description = 'Server instance health object',
           })
             ..required = [
               'name',
               'health',
-            ],
-        )..description = 'Array of server instances',
+            ]
+            ..description = 'Server instance object',
+        )..description = 'Array of server instance objects',
       },
     )
-      ..description = "Module status"
+      ..description = 'Module status object'
       ..isReadOnly = true
       ..additionalPropertyPolicy = APISchemaAdditionalPropertyPolicy.disallowed
       ..required = [
