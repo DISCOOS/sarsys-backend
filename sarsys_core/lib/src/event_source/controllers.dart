3394e6432c0d9f84c5beb56245144c21f0f66ed2
Kenneth Gulbrandsoy <kenneth@discoos.org>
2021-02-22 19:27:08 +0100

    Deleted http-based event source service controllers

--- sarsys_core/lib/src/event_source/controllers.dart
+++ sarsys_core/lib/src/event_source/controllers.dart
@@ -1,9 +1,5 @@
 export 'aggregate_controller.dart';
 export 'aggregate_list_controller.dart';
-export 'aggregate_operations_controller.dart';
 export 'aggregate_lookup_controller.dart';
 export 'entity_controller.dart';
-export 'repository_operations_controller.dart';
-export 'snapshot_file_controller.dart';
-export 'snapshot_operations_controller.dart';
 export 'value_controller.dart';
