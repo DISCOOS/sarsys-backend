c8cff41d9fa01e530b31eab14d879474e3346f42
Kenneth Gulbrandsoy <kenneth@discoos.org>
2021-03-19 20:15:54 +0100

    Fixed duplicate downloads

--- k8s/app/download.sh
+++ k8s/app/download.sh
@@ -3,8 +3,8 @@
 echo "Download sarsys-app-server snapshots..."
 mkdir -p .data
 kubectl -n sarsys cp sarsys/sarsys-app-server-0:/var/lib/sarsys/ .data/server-0
-kubectl -n sarsys cp sarsys/sarsys-app-server-0:/var/lib/sarsys/ .data/server-1
-kubectl -n sarsys cp sarsys/sarsys-app-server-0:/var/lib/sarsys/ .data/server-2
+kubectl -n sarsys cp sarsys/sarsys-app-server-1:/var/lib/sarsys/ .data/server-1
+kubectl -n sarsys cp sarsys/sarsys-app-server-2:/var/lib/sarsys/ .data/server-2
 echo "Analysing folder '.data'"
 ls -al .data
 echo "[✓] Downloading finished"
--- k8s/tracking/download.sh
+++ k8s/tracking/download.sh
@@ -3,8 +3,8 @@
 echo "Download sarsys-tracking-server snapshots..."
 mkdir -p .data
 kubectl -n sarsys cp sarsys/sarsys-tracking-server-0:/var/lib/sarsys/ .data/server-0
-kubectl -n sarsys cp sarsys/sarsys-tracking-server-0:/var/lib/sarsys/ .data/server-1
-kubectl -n sarsys cp sarsys/sarsys-tracking-server-0:/var/lib/sarsys/ .data/server-2
+kubectl -n sarsys cp sarsys/sarsys-tracking-server-1:/var/lib/sarsys/ .data/server-1
+kubectl -n sarsys cp sarsys/sarsys-tracking-server-2:/var/lib/sarsys/ .data/server-2
 echo "Analysing folder '.data'"
 ls -al .data
 echo "[✓] Downloading finished"
